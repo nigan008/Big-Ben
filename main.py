8e99627f6d779a679ffee17172a90aad4335ac95
nigan008 <nigansenthilkumaran07@gmail.com>
2020-09-30 11:49:52 +0530

    sorting teacher's Name

--- main.py
+++ main.py
@@ -15,22 +15,22 @@ a.execute("select * from std_12")
 """data =a.fetchone()
 for i in data:
     print(i) """
+    
+#         Sorting  Teacher's  Name 
 
-def sorting():
-    for m in range(5):
-        data = a.fetchone()
-        for b in range(2,noOfSection+2):
-            teachers.append(data[b])
-        for e in range(0,30):
-            k=[]
-            o=len(teachers)
-            for i in range(e+1,(o)):
-                if teachers[e] == teachers[i]:
-                    k.append(i)
-            k.sort(reverse=True)
-            for p in k:
-                teachers.pop(p)
-sorting()
+for m in range(5):
+    data = a.fetchone()
+    for b in range(2,noOfSection+2):
+        teachers.append(data[b])
+    for e in range(0,30):
+        k=[]
+        o=len(teachers)
+        for i in range(e+1,(o)):
+            if teachers[e] == teachers[i]:
+                k.append(i)
+        k.sort(reverse=True)
+        for p in k:
+            teachers.pop(p)
 for s in teachers :
     print(s)
 time.sleep(5)
