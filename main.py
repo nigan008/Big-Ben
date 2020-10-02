87263ad5a2bc15f11b1a53b1e6fa047f00231fa4
nigan008 <nigansenthilkumaran07@gmail.com>
2020-10-02 15:39:52 +0530

    Added Checking of Existing Tables

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pymysql
 conn=pymysql.connect(host="localhost",user="root",password="",db="project")
 sql=conn.cursor()
-
-
+Find = True
 
 from colorama import init
 init()
@@ -10,109 +9,125 @@ init()
 # Recieving Input From Admin
 
 standard = int(input("STANDARAD : "))
-print("")
-noOfSubject = int(input("No . Of . Subject :"))
-print("")
-noOfSection = int(input("No . Of. Section :"))
 
-# Assining Name Of Section
-
-listOfSecetion = []
-for i in range(noOfSection):
-    alphabet = "Section_" + chr( i + 65)
-    listOfSecetion.append(alphabet)
-
-# Recieving Name Of The Subject From Admin
-
-listOfSubject = []
-
-print("\033[2J")
-print("Please Enter The Name Of Subjects")
-for i in range(noOfSubject):
-    print('')
-    subject = input(f"Name Of Subject {i + 1} :")
-    listOfSubject.append(subject)
-
-# Recieving No Of Periods per Week for Each Subject From Admin
-
-list_Of_No_Of_Periods = []
-
-print("\033[2J")
-print("Please Enter The No of Periods Per Week According To Thier Subject")
-
-for i in listOfSubject:
-    noOfPeriod = int(input(f"{i}"))
-    list_Of_No_Of_Periods.append(noOfPeriod) 
-
-#  Recieving Class Info
-
-classInfo = []
-
-for i in listOfSubject :
-    print("\033[2J")
-    print (f"Please Enter The  Name Of  {i} Teacher According To Thier Section ")
-
-    # Appending Name Of Subject And NoOf Periods 
-    nameOfTeachers = []
-    nameOfTeachers.append(i)
-    nameOfTeachers.append(list_Of_No_Of_Periods[listOfSubject.index(i)])
-    
-
-    # Recieving Name Of the Teacher
-
-    for i in listOfSecetion:
-        nameOfTeacher = input(f"{i} :")
-        nameOfTeachers.append(nameOfTeacher)
-
-    classInfo.append(nameOfTeachers)
-
-# Creation Of Table " Class Info "
-
-
-sql.execute(f"create table class_{standard} (Subject varchar(50) , No_Of_Periods  int(10) );")
-
-for i in listOfSecetion:
-    sql.execute(f"alter table class_{standard} add ({i} varchar(50));")
-
-String = ("%s," * (noOfSection + 1))
-
-for i in classInfo:
-    command = "insert into class_" + str(standard) + " values (" + String +"%s);"
-    value = i
-    sql.execute(command,value)
-
-#         Sorting  Teacher's  Name 
-
-teachers = []
-sql.execute(f"select * from class_{standard}")
-
-for m in range(5):
-    data = sql.fetchone()
-    for b in range(2,noOfSection+2):
-        teachers.append(data[b])
-    for e in range(0,30):
-        k=[]
-        o=len(teachers)
-        for i in range(e+1,(o)):
-            if teachers[e] == teachers[i]:
-                k.append(i)
-        k.sort(reverse=True)
-        for p in k:
-            teachers.pop(p)
-for s in teachers :
-    print(s)
-#  Table Verification 
-
+# To Find The Existence Of Tables :
 sql.execute("show tables")
 data = sql.fetchall()
-sortedTables = []
 for i in data:
-    if i[0][0 : 7] == "teacher":
-        sortedTables.append(i[0])
-      
-#          Teacher's ID Creation
+    if i[0] == "class_" + str(standard):
+        print(f"The Time Table for class_{standard} was Already Ceated ")
+        Find = False
+        descision = input("Do You Want to Reshedule It !! y/n")
+        if descision == "y" or "Y":
+            pass
 
-def checking(teacher):
+        elif descision == "n" or "N":
+            pass
+        
+
+if Find:
+    print("")
+    noOfSubject = int(input("No . Of . Subject :"))
+    print("")
+    noOfSection = int(input("No . Of. Section :"))
+
+    # Assining Name Of Section
+
+    listOfSecetion = []
+    for i in range(noOfSection):
+        alphabet = "Section_" + chr( i + 65)
+        listOfSecetion.append(alphabet)
+
+    # Recieving Name Of The Subject From Admin
+
+    listOfSubject = []
+
+    print("\033[2J")
+    print("Please Enter The Name Of Subjects")
+    for i in range(noOfSubject):
+        print('')
+        subject = input(f"Name Of Subject {i + 1} :")
+        listOfSubject.append(subject)
+
+    # Recieving No Of Periods per Week for Each Subject From Admin
+
+    list_Of_No_Of_Periods = []
+
+    print("\033[2J")
+    print("Please Enter The No of Periods Per Week According To Thier Subject")
+
+    for i in listOfSubject:
+        noOfPeriod = int(input(f"{i}"))
+        list_Of_No_Of_Periods.append(noOfPeriod) 
+
+    #  Recieving Class Info
+
+    classInfo = []
+
+    for i in listOfSubject :
+        print("\033[2J")
+        print (f"Please Enter The  Name Of  {i} Teacher According To Thier Section ")
+
+        # Appending Name Of Subject And NoOf Periods 
+
+        nameOfTeachers = []
+        nameOfTeachers.append(i)
+        nameOfTeachers.append(list_Of_No_Of_Periods[listOfSubject.index(i)])
+        
+        # Recieving Name Of the Teacher
+
+        for i in listOfSecetion:
+            nameOfTeacher = input(f"{i} :")
+            nameOfTeachers.append(nameOfTeacher)
+
+        classInfo.append(nameOfTeachers)
+
+    # Creation Of Table " Class Info "
+
+
+    sql.execute(f"create table class_{standard} (Subject varchar(50) , No_Of_Periods  int(10) );")
+
+    for i in listOfSecetion:
+        sql.execute(f"alter table class_{standard} add ({i} varchar(50));")
+
+    String = ("%s," * (noOfSection + 1))
+
+    for i in classInfo:
+        command = "insert into class_" + str(standard) + " values (" + String +"%s);"
+        value = i
+        sql.execute(command,value)
+
+    #         Sorting  Teacher's  Name 
+
+    teachers = []
+    sql.execute(f"select * from class_{standard}")
+
+    for m in range(5):
+        data = sql.fetchone()
+        for b in range(2,noOfSection+2):
+            teachers.append(data[b])
+        for e in range(0,30):
+            k=[]
+            o=len(teachers)
+            for i in range(e+1,(o)):
+                if teachers[e] == teachers[i]:
+                    k.append(i)
+            k.sort(reverse=True)
+            for p in k:
+                teachers.pop(p)
+    for s in teachers :
+        print(s)
+
+    #  Table Verification 
+
+    sql.execute("show tables")
+    data = sql.fetchall()
+    sortedTables = []
+    for i in data:
+        if i[0][0 : 7] == "teacher":
+            sortedTables.append(i[0])
+        
+    #          Teacher's ID Creation
 
     if not sortedTables == []:
         oldTeacher=[]
@@ -125,18 +140,18 @@ def checking(teacher):
             lastId = data[len(data)-1][0]
             
             removingTeacher = []
-            for i in teacher:
+            for i in teachers:
                 for j in range(len(data)):
                     if i == data[j][1]:
                         removingTeacher.append(i)
                         oldTeacher.append(data[j])
             for i in removingTeacher:
-                teacher.remove(i)
+                teachers.remove(i)
 
         #Creating new Id for New Teacher
 
         newIdList = []
-        for i in range(1,(len(teacher)+1)):
+        for i in range(1,(len(teachers)+1)):
             Id = lastId + i
             newIdList.append(Id)
 
@@ -145,10 +160,10 @@ def checking(teacher):
         teacher_info = []
         for i in oldTeacher :
             teacher_info.append(i) 
-        for i in range(len(teacher)):
+        for i in range(len(teachers)):
             temp = []
             temp.append(newIdList[i])
-            temp.append(teacher[i])
+            temp.append(teachers[i])
             teacher_info.append(temp)
         print(teacher_info)
 
@@ -159,18 +174,20 @@ def checking(teacher):
             Command = "insert into teacher_" + str(standard) + " values(%s,%s)"
             Value = i
             sql.execute(Command,Value)
+
     else :
+
         IdList = []
         teacher_info = []
     # Creating  Id For Teacher
-        for i in range(len(teacher)):
+        for i in range(len(teachers)):
             Id = 1000 + i
             IdList.append(Id)
     # Combining Name And Id
-        for i in range(len(teacher)):
+        for i in range(len(teachers)):
             temp = []
             temp.append(IdList[i])
-            temp.append(teacher[i])
+            temp.append(teachers[i])
             teacher_info.append(temp)
         print(teacher_info)
 
@@ -181,8 +198,8 @@ def checking(teacher):
             Command = f"insert into teacher_" + str(standard) + " values(%s,%s)"
             Value = i
             sql.execute(Command,Value)
-        
-checking(teachers)
+            
+    
 
 
 
